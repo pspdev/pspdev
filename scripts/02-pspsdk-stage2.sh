69679d4a1dde5be2943ba68ee38323fb963b24fc
Carsten Teibes <dev@f4ke.de>
2020-05-25 23:34:43 +0200

    Use cleaned up pspsdk

--- scripts/02-pspsdk-stage2.sh
+++ scripts/02-pspsdk-stage2.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Download the source code if it does not already exist.
-clone_git_repo github.com pspdev pspsdk
+clone_git_repo github.com pspdev pspsdk cleanup
 
 # Enter the source directory.
 cd pspsdk
