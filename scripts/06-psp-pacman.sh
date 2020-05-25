b638ffdd8f5636dee22e3933266ade23723ee5a6
Carsten Teibes <dev@f4ke.de>
2020-05-25 18:25:27 +0200

    Add pacman build step

---
+++ scripts/06-psp-pacman.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# psp-pacman.sh
+
+# Exit on errors
+set -e
+
+# Download the source code.
+clone_git_repo github.com pspdev psp-pacman
+
+# Enter the source directory.
+cd psp-pacman
+
+# Build and install
+./pacman.sh
