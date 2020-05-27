3d234dcb25c20e7c45f176f2cdfbbcb1f892d937
Carsten Teibes <dev@f4ke.de>
2020-05-27 03:01:16 +0200

    Change pacman script to ci branch

--- scripts/06-psp-pacman.sh
+++ scripts/06-psp-pacman.sh
@@ -5,7 +5,7 @@
 set -e
 
 # Download the source code.
-clone_git_repo github.com pspdev psp-pacman old-ubuntu
+clone_git_repo github.com pspdev psp-pacman ci
 
 # Enter the source directory.
 cd psp-pacman
