d4701b57592232f91a4dc3f94f76595fc149a533
jopadan <jopadan@ctemplar.com>
2020-07-26 22:29:44 +0200

    fix octal to decimal in printf

--- pspdev.sh
+++ pspdev.sh
@@ -33,7 +33,7 @@ BUILD_SCRIPTS=(`ls ../scripts/*.sh | sort`)
 # If specific steps were requested...
 if [ $1 ]; then
   # Sort and run the requested build scripts.
-  ARGS=(`printf "%.2d\n" $* | sort -n`)
+  ARGS=(`printf "%.2d\n" $((10#$*)) | sort -n`)
 
   for ARG in ${ARGS[@]}; do
     found=0
