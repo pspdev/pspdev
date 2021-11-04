28400e4d65f6a01a703355ac8756e230c4472ac1
Jon Daniel <jondaniel879@gmail.com>
2021-11-04 19:41:35 +0100

    revert INSTALLDIR default

--- pspdev-sudo.sh
+++ pspdev-sudo.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # pspdev-sudo.sh
  
-INSTALLDIR=/opt/toolchains/psp/jopadan
+INSTALLDIR=/usr/local/pspdev
  
 # Enter the pspdev directory
 cd "`dirname $0`" || { echo "ERROR: Could not enter the pspdev directory."; exit $(false); }
