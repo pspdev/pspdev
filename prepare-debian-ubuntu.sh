793a3f081aced6efe8a2c599c4cb65a12acbe3a5
draanPSP <80162347+draanPSP@users.noreply.github.com>
2021-11-19 18:06:58 +0100

    Added missing packages to the install script

--- prepare-debian-ubuntu.sh
+++ prepare-debian-ubuntu.sh
@@ -4,4 +4,4 @@
 sudo apt-get install $@ \
   git gzip libarchive-dev libcurl4 libcurl4-openssl-dev libelf-dev libgpgme-dev libncurses5-dev libreadline-dev libssl-dev \
   libtool-bin libusb-dev m4 make patch pkg-config python3 python3-venv subversion tar tcl texinfo unzip wget xz-utils \
-  sudo fakeroot libarchive-tools curl libgmp3-dev libmpfr-dev libmpc-dev python3-pip
+  sudo fakeroot libarchive-tools curl libgmp3-dev libmpfr-dev libmpc-dev python3-pip autoconf automake bison bzip2 cmake doxygen flex g++ gcc
