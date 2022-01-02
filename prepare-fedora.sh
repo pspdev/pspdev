fa888f1e85fc726cce3761e601b2f587b3664b3f
David Guillen Fandos <david@davidgf.net>
2022-01-02 17:49:29 +0100

    Add required package fakeroot
    This is required for psp-packages builds but missing from the Fedora
    script.

--- prepare-fedora.sh
+++ prepare-fedora.sh
@@ -2,6 +2,6 @@
 
 # Install build dependencies
 sudo dnf install $@ \
-  @development-tools gcc gcc-c++ g++ wget git autoconf automake python3 python3-pip make cmake pkgconf \
+  @development-tools gcc gcc-c++ g++ wget git autoconf automake python3 python3-pip make cmake pkgconf fakeroot \
   libarchive-devel openssl-devel gpgme-devel libtool gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel ncurses-devel diffutils \
   libusb-devel readline-devel
