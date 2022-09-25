e1bab6e2a1416fdd42200321d2dcec3351f43ab9
Sadeeed <sadeeedw@gmail.com>
2022-09-25 11:41:02 +0500

    add arch prepare script

---
+++ prepare-arch.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# Install build dependencies
+sudo pacman -Sy gcc clang make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool
