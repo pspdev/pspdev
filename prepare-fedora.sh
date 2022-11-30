9b2713ecb27d00f46228278ac5a83b1ac3430146
Mark <markedenergy@gmail.com>
2022-11-30 11:52:00 +0100

    `prepare-fedora.sh`: fix broken package
    `libusb-devel` does not exist anymore. The correct package is now `libusb-compat-0.1-devel`.

--- prepare-fedora.sh
+++ prepare-fedora.sh
@@ -4,4 +4,4 @@
 sudo dnf install $@ \
   @development-tools gcc gcc-c++ g++ wget git autoconf automake python3 python3-pip make cmake pkgconf fakeroot \
   libarchive-devel openssl-devel gpgme-devel libtool gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel ncurses-devel diffutils \
-  libusb-devel readline-devel
+  libusb-compat-0.1-devel readline-devel
