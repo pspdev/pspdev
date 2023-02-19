12502a5320827b010c8eaddbffa0bf7d4bf6d88d
Diogo Autilio <diautilio@gmail.com>
2023-02-19 11:23:09 -0300

    Update prepare-arch.sh
    Add missing pip3 dependency

--- prepare-arch.sh
+++ prepare-arch.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Install build dependencies
-sudo pacman -Sy gcc clang make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool libusb-compat boost
+sudo pacman -Sy gcc clang make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool libusb-compat boost python-pip
+
