8f3866da58d46c2b570479d781a710ce031d1dae
wally4000 <catapultit@me.com>
2024-04-30 10:28:30 +1000

    Fix Arch line

--- prepare.sh
+++ prepare.sh
@@ -29,6 +29,7 @@ else
     ;;
     arch)
         sudo pacman -Sy gcc clang make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool libusb-compat boost python-pip
+    ;;
     *)
         echo "$TESTOS not supported here"
     ;;
