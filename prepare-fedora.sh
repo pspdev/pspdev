b394ade290ebe0175ef5f946d85b27f4fcb49b81
wally4000 <catapultit@me.com>
2024-04-30 10:26:23 +1000

    Replace all prepare scripts with a singular script and use in workflow

--- prepare-fedora.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Install build dependencies
-sudo dnf install $@ \
-  @development-tools gcc gcc-c++ g++ wget git autoconf automake python3 python3-pip make cmake pkgconf fakeroot \
-  libarchive-devel openssl-devel gpgme-devel libtool gettext texinfo bison flex gmp-devel mpfr-devel libmpc-devel ncurses-devel diffutils \
-  libusb-compat-0.1-devel readline-devel libcurl-devel
