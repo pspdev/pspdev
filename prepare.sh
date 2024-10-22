bdc99165457d757cf0ee1caf24f81eccad6f2971
Wouter Wijsman <wwijsman@live.nl>
2024-10-22 13:40:53 +0200

    Add Manjaro support to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -34,7 +34,7 @@ else
                                         app-arch/libarchive app-crypt/gpgme sys-devel/bison sys-devel/flex\
                                         dev-libs/mpc dev-libs/libusb-compat
     ;;
-    arch)
+    arch | manjaro)
         sudo pacman -Sy gcc clang make cmake patch git texinfo flex bison gettext wget gsl gmp mpfr libmpc libusb readline libarchive gpgme bash openssl libtool libusb-compat boost python-pip
     ;;
     *)
